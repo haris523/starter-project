8bfea5bf633ba7ee5f6aadd4f2e4b43f36d7cabf
Ahmed Suljic <handzicharis62@yahoo.com>
2021-12-02 11:16:59 +0100

    Updated react native version
    Updated react-native version from 0.65.1 to 0.66.3
    Updated most of the dependencies to the newest version aswell
    Added enableFreeze call to App.js for out of the box optimization (Comment in if causes issues)

--- template/App.tsx
+++ template/App.tsx
@@ -1,22 +1,36 @@
-import { Text } from 'components'
-import React, { useEffect } from 'react'
-import { View, StyleSheet } from 'react-native'
-import SplashScreen from 'react-native-splash-screen'
+import { Text } from "components";
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { enableFreeze } from "react-native-screens";
+import SplashScreen from "react-native-splash-screen";
+
+// This is for optimization, every screen that's not focused has a never ending promise
+// that stalls it, which means that whatever the app would like to rerender in the background
+// would now be unable to be rerendered. Comment it if it causes any issues.
+enableFreeze(true);
 
 const App = () => {
-    useEffect(()=>{
-        SplashScreen.hide()
-    }, [])
-    return (
-        <View style={{ flex:1, alignItems:'center', justifyContent:'center', backgroundColor:'white', padding:40 }}>
-            <Text style={localStyles.text}>Starter project</Text>
-        </View>
-    )
-}
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+        padding: 40,
+      }}
+    >
+      <Text style={localStyles.text}>Starter project</Text>
+    </View>
+  );
+};
 
 const localStyles = StyleSheet.create({
-    text:{
-        textAlign:'center'
-    }
-})
-export default App
+  text: {
+    textAlign: "center",
+  },
+});
+export default App;
